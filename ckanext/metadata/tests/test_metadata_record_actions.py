19e2c9fe5fe455ef27eef23b07a40bb035cc5d0b
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-04-17 14:32:56 +0200

    Split up test routines into multiple files

---
+++ ckanext/metadata/tests/test_metadata_record_actions.py
@@ -0,0 +1,347 @@
+# encoding: utf-8
+
+from ckan.tests import factories as ckan_factories
+from ckan.tests.helpers import call_action
+import ckan.plugins.toolkit as tk
+import ckan.model as ckan_model
+
+from ckanext.metadata.tests import (
+    ActionTestBase,
+    make_uuid,
+    assert_package_has_extra,
+    assert_group_has_member,
+    assert_error,
+    factories as ckanext_factories,
+)
+
+
+class TestMetadataRecordActions(ActionTestBase):
+
+    def setup(self):
+        super(TestMetadataRecordActions, self).setup()
+        self.owner_org = ckan_factories.Organization(
+            users=[{'name': self.normal_user['name'], 'capacity': 'editor'}])
+        self.metadata_collection = ckanext_factories.MetadataCollection(organization_id=self.owner_org['id'])
+        self.metadata_schema = ckanext_factories.MetadataSchema()
+
+    def _generate_infrastructure(self):
+        return ckanext_factories.Infrastructure(
+            users=[{'name': self.normal_user['name'], 'capacity': 'member'}]
+        )
+
+    def _generate_metadata_record(self, **kwargs):
+        return ckanext_factories.MetadataRecord(
+            owner_org=self.owner_org['id'],
+            metadata_collection_id=self.metadata_collection['id'],
+            **kwargs
+        )
+
+    def _make_input_dict(self):
+        return {
+            'title': 'Test Metadata Record',
+            'owner_org': self.owner_org['id'],
+            'metadata_collection_id': self.metadata_collection['id'],
+            'infrastructures': [],
+            'schema_name': self.metadata_schema['schema_name'],
+            'schema_version': self.metadata_schema['schema_version'],
+            'content_json': '{ "testkey": "testvalue" }',
+            'content_raw': '<xml/>',
+            'content_url': 'http://example.net/',
+        }
+
+    def _assert_metadata_record_ok(self, obj, input_dict, **kwargs):
+        """
+        Checks the resulting package object against the input dict and referenced objects.
+        Override comparison values using kwargs.
+        """
+        assert obj.type == 'metadata_record'
+        assert obj.title == kwargs.pop('title', input_dict.get('title'))
+        assert obj.name == kwargs.pop('name', 'metadata-' + obj.id)
+        assert obj.owner_org == kwargs.pop('owner_org', self.owner_org['id'])
+        assert_package_has_extra(obj.id, 'metadata_collection_id', kwargs.pop('metadata_collection_id', self.metadata_collection['id']))
+        assert_package_has_extra(obj.id, 'metadata_schema_id', kwargs.pop('metadata_schema_id', self.metadata_schema['id']))
+        assert_package_has_extra(obj.id, 'content_json', input_dict['content_json'])
+        assert_package_has_extra(obj.id, 'content_raw', input_dict['content_raw'])
+        assert_package_has_extra(obj.id, 'content_url', input_dict['content_url'])
+
+    def test_create_valid(self):
+        input_dict = self._make_input_dict()
+        result, obj = self._call_action('create', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+        self._assert_metadata_record_ok(obj, input_dict)
+
+    def test_create_valid_setname(self):
+        input_dict = self._make_input_dict()
+        input_dict['name'] = 'test-metadata-record'
+        result, obj = self._call_action('create', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+        self._assert_metadata_record_ok(obj, input_dict, name=input_dict['name'])
+
+    def test_create_valid_owner_org_byname(self):
+        input_dict = self._make_input_dict()
+        input_dict['owner_org'] = self.owner_org['name']
+        result, obj = self._call_action('create', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+        self._assert_metadata_record_ok(obj, input_dict)
+
+    def test_create_valid_with_infrastructures(self):
+        infrastructure1 = self._generate_infrastructure()
+        infrastructure2 = self._generate_infrastructure()
+        input_dict = self._make_input_dict()
+        input_dict.update({
+            'infrastructures': [
+                {'id': infrastructure1['id']},
+                {'id': infrastructure2['name']},
+            ],
+        })
+        result, obj = self._call_action('create', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+        self._assert_metadata_record_ok(obj, input_dict)
+        assert_group_has_member(infrastructure1['id'], obj.id, 'package')
+        assert_group_has_member(infrastructure2['id'], obj.id, 'package')
+
+    def test_create_valid_sysadmin_setid(self):
+        input_dict = self._make_input_dict()
+        input_dict['id'] = make_uuid()
+        result, obj = self._call_action('create', 'metadata_record',
+                                        model_class=ckan_model.Package,
+                                        sysadmin=True, check_auth=True, **input_dict)
+        self._assert_metadata_record_ok(obj, input_dict)
+
+    def test_create_invalid_nonsysadmin_setid(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError, check_auth=True,
+                                        id=make_uuid())
+        assert_error(result, 'id', 'The input field id was not expected.')
+
+    def test_create_invalid_sysadmin_duplicate_id(self):
+        metadata_record = ckanext_factories.MetadataRecord()
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError, sysadmin=True, check_auth=True,
+                                        id=metadata_record['id'])
+        assert_error(result, 'id', 'Dataset id already exists')
+
+    def test_create_invalid_missing_params(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError)
+        assert_error(result, 'owner_org', 'Missing parameter')
+        assert_error(result, 'metadata_collection_id', 'Missing parameter')
+        assert_error(result, 'infrastructures', 'Missing parameter')
+        assert_error(result, 'schema_name', 'Missing parameter')
+        assert_error(result, 'schema_version', 'Missing parameter')
+        assert_error(result, 'content_json', 'Missing parameter')
+        assert_error(result, 'content_raw', 'Missing parameter')
+        assert_error(result, 'content_url', 'Missing parameter')
+
+    def test_create_invalid_missing_values(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        owner_org='',
+                                        metadata_collection_id='',
+                                        schema_name='',
+                                        schema_version='')
+        assert_error(result, 'owner_org', 'Missing value')
+        assert_error(result, 'metadata_collection_id', 'Missing value')
+        assert_error(result, 'schema_name', 'Missing value')
+        assert_error(result, 'schema_version', 'Missing value')
+
+    def test_create_invalid_duplicate_name(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        name=metadata_record['name'])
+        assert_error(result, 'name', 'That URL is already in use.')
+
+    def test_create_invalid_not_json(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        content_json='not json')
+        assert_error(result, 'content_json', 'JSON decode error')
+
+    def test_create_invalid_not_json_dict(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        content_json='[1,2,3]')
+        assert_error(result, 'content_json', 'Expecting a JSON dictionary')
+
+    def test_create_invalid_bad_references(self):
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        owner_org='a',
+                                        metadata_collection_id='b',
+                                        schema_name='c',
+                                        schema_version='d',
+                                        infrastructures=[{'id': 'e'}])
+        assert_error(result, 'owner_org', 'Not found: Organization')
+        assert_error(result, 'metadata_collection_id', 'Not found: Metadata Collection')
+        assert_error(result, '__after', 'Could not find a metadata schema')
+        assert_error(result['infrastructures'][0], 'id', 'Not found: Infrastructure')
+
+    def test_create_invalid_deleted_references(self):
+        infrastructure = self._generate_infrastructure()
+        call_action('organization_delete', id=self.owner_org['id'])
+        call_action('metadata_collection_delete', id=self.metadata_collection['id'])
+        call_action('metadata_schema_delete', id=self.metadata_schema['id'])
+        call_action('infrastructure_delete', id=infrastructure['id'])
+
+        input_dict = self._make_input_dict()
+        input_dict['infrastructures'] = [{'id': infrastructure['id']}]
+        result, obj = self._call_action('create', 'metadata_record',
+                                        exception_class=tk.ValidationError, **input_dict)
+
+        assert_error(result, 'owner_org', 'Not found: Organization')
+        assert_error(result, 'metadata_collection_id', 'Not found: Metadata Collection')
+        assert_error(result, '__after', 'Could not find a metadata schema')
+        assert_error(result['infrastructures'][0], 'id', 'Not found: Infrastructure')
+
+    def test_update_valid(self):
+        infrastructure1 = self._generate_infrastructure()
+        infrastructure2 = self._generate_infrastructure()
+        metadata_record = self._generate_metadata_record(infrastructures=[
+            {'id': infrastructure1['id']}, {'id': infrastructure2['id']}])
+
+        new_metadata_collection = ckanext_factories.MetadataCollection(organization_id=self.owner_org['id'])
+        new_metadata_schema = ckanext_factories.MetadataSchema()
+        new_infrastructure = self._generate_infrastructure()
+
+        input_dict = {
+            'id': metadata_record['id'],
+            'name': 'updated-test-metadata-record',
+            'title': 'Updated Test Metadata Record',
+            'owner_org': self.owner_org['id'],
+            'metadata_collection_id': new_metadata_collection['id'],
+            'schema_name': new_metadata_schema['schema_name'],
+            'schema_version': new_metadata_schema['schema_version'],
+            'content_json': '{ "newtestkey": "newtestvalue" }',
+            'content_raw': '<updated_xml/>',
+            'content_url': 'http://updated.example.net/',
+            'infrastructures': [
+                {'id': infrastructure2['name']},
+                {'id': new_infrastructure['name']},
+            ],
+        }
+        result, obj = self._call_action('update', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+
+        self._assert_metadata_record_ok(obj, input_dict,
+                                        name=input_dict['name'],
+                                        metadata_collection_id=new_metadata_collection['id'],
+                                        metadata_schema_id=new_metadata_schema['id'])
+        assert_group_has_member(infrastructure1['id'], obj.id, 'package', state='deleted')
+        assert_group_has_member(infrastructure2['id'], obj.id, 'package')
+        assert_group_has_member(new_infrastructure['id'], obj.id, 'package')
+
+    def test_update_valid_partial(self):
+        infrastructure = self._generate_infrastructure()
+        metadata_record = self._generate_metadata_record(
+            infrastructures=[{'id': infrastructure['id']}])
+
+        input_dict = self._make_input_dict()
+        input_dict.update({
+            'id': metadata_record['id'],
+            'name': 'updated-test-metadata-record',
+            'content_json': '{ "newtestkey": "newtestvalue" }',
+            'infrastructures': [{'id': infrastructure['id']}],
+        })
+        del input_dict['title']
+
+        result, obj = self._call_action('update', 'metadata_record',
+                                        model_class=ckan_model.Package, **input_dict)
+
+        self._assert_metadata_record_ok(obj, input_dict,
+                                        name=input_dict['name'],
+                                        title=metadata_record['title'])
+        assert_group_has_member(infrastructure['id'], obj.id, 'package')
+
+    def test_update_invalid_duplicate_name(self):
+        metadata_record1 = self._generate_metadata_record()
+        metadata_record2 = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record1['id'],
+                                        name=metadata_record2['name'])
+        assert_error(result, 'name', 'That URL is already in use.')
+
+    def test_update_invalid_missing_params(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record['id'])
+        assert_error(result, 'owner_org', 'Missing parameter')
+        assert_error(result, 'metadata_collection_id', 'Missing parameter')
+        assert_error(result, 'infrastructures', 'Missing parameter')
+        assert_error(result, 'schema_name', 'Missing parameter')
+        assert_error(result, 'schema_version', 'Missing parameter')
+        assert_error(result, 'content_json', 'Missing parameter')
+        assert_error(result, 'content_raw', 'Missing parameter')
+        assert_error(result, 'content_url', 'Missing parameter')
+
+    def test_update_invalid_missing_values(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record['id'],
+                                        owner_org='',
+                                        metadata_collection_id='',
+                                        schema_name='',
+                                        schema_version='')
+        assert_error(result, 'owner_org', 'Missing value')
+        assert_error(result, 'metadata_collection_id', 'Missing value')
+        assert_error(result, 'schema_name', 'Missing value')
+        assert_error(result, 'schema_version', 'Missing value')
+
+    def test_update_invalid_not_json(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record['id'],
+                                        content_json='not json')
+        assert_error(result, 'content_json', 'JSON decode error')
+
+    def test_update_invalid_not_json_dict(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record['id'],
+                                        content_json='[1,2,3]')
+        assert_error(result, 'content_json', 'Expecting a JSON dictionary')
+
+    def test_update_invalid_bad_references(self):
+        metadata_record = self._generate_metadata_record()
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError,
+                                        id=metadata_record['id'],
+                                        owner_org='a',
+                                        metadata_collection_id='b',
+                                        schema_name='c',
+                                        schema_version='d',
+                                        infrastructures=[{'id': 'e'}])
+        assert_error(result, 'owner_org', 'Not found: Organization')
+        assert_error(result, 'metadata_collection_id', 'Not found: Metadata Collection')
+        assert_error(result, '__after', 'Could not find a metadata schema')
+        assert_error(result['infrastructures'][0], 'id', 'Not found: Infrastructure')
+
+    def test_update_invalid_deleted_references(self):
+        metadata_record = self._generate_metadata_record()
+        infrastructure = self._generate_infrastructure()
+        call_action('organization_delete', id=self.owner_org['id'])
+        call_action('metadata_collection_delete', id=self.metadata_collection['id'])
+        call_action('metadata_schema_delete', id=self.metadata_schema['id'])
+        call_action('infrastructure_delete', id=infrastructure['id'])
+
+        input_dict = self._make_input_dict()
+        input_dict['id'] = metadata_record['id']
+        input_dict['infrastructures'] = [{'id': infrastructure['id']}]
+        result, obj = self._call_action('update', 'metadata_record',
+                                        exception_class=tk.ValidationError, **input_dict)
+
+        assert_error(result, 'owner_org', 'Not found: Organization')
+        assert_error(result, 'metadata_collection_id', 'Not found: Metadata Collection')
+        assert_error(result, '__after', 'Could not find a metadata schema')
+        assert_error(result['infrastructures'][0], 'id', 'Not found: Infrastructure')
+
+    def test_delete_valid(self):
+        metadata_record = self._generate_metadata_record()
+        self._call_action('delete', 'metadata_record',
+                          model_class=ckan_model.Package,
+                          id=metadata_record['id'])
---
+++ ckanext/metadata/tests/test_organization_actions.py
@@ -0,0 +1,77 @@
+# encoding: utf-8
+
+from ckan import model as ckan_model
+from ckan.plugins import toolkit as tk
+from ckan.tests import factories as ckan_factories
+from ckan.tests.helpers import call_action
+
+from ckanext.metadata import model as ckanext_model
+from ckanext.metadata.tests import (
+    ActionTestBase,
+    assert_error,
+    factories as ckanext_factories,
+)
+
+
+class TestOrganizationActions(ActionTestBase):
+
+    # TODO: most of these tests will not work, as chaining of action functions is broken in CKAN
+    # and therefore we cannot correctly implement our organization_delete override
+
+    def test_delete_valid(self):
+        organization = ckan_factories.Organization()
+        self._call_action('delete', 'organization',
+                          model_class=ckan_model.Group,
+                          id=organization['id'])
+
+    def test_delete_valid_cascade_metadata_models(self):
+        organization = ckan_factories.Organization()
+        metadata_model = ckanext_factories.MetadataModel(organization_id=organization['id'])
+
+        self._call_action('delete', 'organization',
+                          model_class=ckan_model.Group,
+                          id=organization['id'])
+        assert ckanext_model.MetadataModel.get(metadata_model['id']).state == 'deleted'
+
+    def test_delete_valid_cascade_metadata_collections(self):
+        organization = ckan_factories.Organization()
+        metadata_collection = ckanext_factories.MetadataCollection(organization_id=organization['id'])
+
+        self._call_action('delete', 'organization',
+                          model_class=ckan_model.Group,
+                          id=organization['id'])
+        assert ckan_model.Group.get(metadata_collection['id']).state == 'deleted'
+
+    def test_delete_with_dependent_metadata_records(self):
+        organization = ckan_factories.Organization()
+        metadata_collection = ckanext_factories.MetadataCollection(organization_id=organization['id'])
+        metadata_record = ckanext_factories.MetadataRecord(owner_org=organization['id'],
+                                                           metadata_collection_id=metadata_collection['id'])
+
+        result, obj = self._call_action('delete', 'organization',
+                                        exception_class=tk.ValidationError,
+                                        id=organization['id'])
+        assert_error(result, 'message', 'Organization has dependent metadata records')
+
+        call_action('metadata_record_delete', id=metadata_record['id'])
+        self._call_action('delete', 'organization',
+                          model_class=ckan_model.Group,
+                          id=organization['id'])
+        assert ckan_model.Group.get(metadata_collection['id']).state == 'deleted'
+
+    def test_delete_with_dependent_metadata_models(self):
+        # TODO: this test will work once we have metadata models being referenced for validation
+        organization = ckan_factories.Organization()
+        metadata_model = ckanext_factories.MetadataModel(organization_id=organization['id'])
+
+        # add validation objects here
+        result, obj = self._call_action('delete', 'organization',
+                                        exception_class=tk.ValidationError,
+                                        id=organization['id'])
+        assert_error(result, 'message', 'Organization has dependent metadata models that are in use')
+
+        # delete validation objects here
+        self._call_action('delete', 'organization',
+                          model_class=ckan_model.Group,
+                          id=organization['id'])
+        assert ckanext_model.MetadataModel.get(metadata_model['id']).state == 'deleted'
