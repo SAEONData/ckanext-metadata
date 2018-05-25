de1d3493d418d8fa5c1d972dff06dccd459a11f7
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-05-25 14:30:46 +0200

    Update travis config
    to get automatic builds running properly

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,12 +5,12 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty
+sudo apt-get install solr-jetty
 
-echo "Installing CKAN $CKAN_RELEASE and its Python dependencies..."
-wget https://github.com/ckan/ckan/archive/ckan-$CKAN_RELEASE.tar.gz
-tar xzf ckan-$CKAN_RELEASE.tar.gz
-mv ckan-ckan-$CKAN_RELEASE ckan
+echo "Installing CKAN $CKANVERSION and its Python dependencies..."
+wget https://github.com/ckan/ckan/archive/ckan-$CKANVERSION.tar.gz
+tar xzf ckan-$CKANVERSION.tar.gz
+mv ckan-ckan-$CKANVERSION ckan
 cd ckan
 pip install -e .
 pip install -r requirements.txt --allow-all-external
@@ -25,6 +25,9 @@ echo "SOLR config..."
 # Solr is multicore for tests on ckan master, but it's easier to run tests on
 # Travis single-core. See https://github.com/ckan/ckan/issues/2972
 sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 
 echo "Initialising the database..."
 cd ckan
