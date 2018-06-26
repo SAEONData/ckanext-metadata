262bac79cd52535dd41c5be1c218f85666414d0d
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-06-26 14:29:25 +0200

    Include extension dependencies in travis config

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -40,6 +40,7 @@ cd -
 
 echo "Installing ckanext-metadata and its requirements..."
 python setup.py develop
+pip install -r requirements.txt
 pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
