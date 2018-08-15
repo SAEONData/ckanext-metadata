13f2eaea2ed61dfb35917ea3af22b66970f1c550
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-08-15 11:01:02 +0200

    Install ckanext-jsonpatch during travis build; ref #36

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -8,9 +8,8 @@ sudo apt-get update -qq
 sudo apt-get install solr-jetty
 
 echo "Installing CKAN v$CKAN_VERSION and its Python dependencies..."
-wget https://github.com/ckan/ckan/archive/ckan-$CKAN_VERSION.tar.gz
-tar xzf ckan-$CKAN_VERSION.tar.gz
-mv ckan-ckan-$CKAN_VERSION ckan
+wget https://github.com/ckan/ckan/archive/ckan-$CKAN_VERSION.tar.gz -O ckan.tar.gz
+tar xzf ckan.tar.gz
 cd ckan
 pip install -e .
 sed -i 's/psycopg2.*/psycopg2==2.7.3.2/' requirements.txt
@@ -44,9 +43,9 @@ pip install -r requirements.txt
 pip install -r dev-requirements.txt
 
 echo "Installing ckanext-jsonpatch v$JSONPATCH_VERSION and its requirements..."
-wget https://github.com/SAEONData/ckanext-jsonpatch/archive/v$JSONPATCH_VERSION.tar.gz
-tar xzf ckanext-jsonpatch-$JSONPATCH_VERSION.tar.gz
-cd ckanext-jsonpatch-$JSONPATCH_VERSION
+wget https://github.com/SAEONData/ckanext-jsonpatch/archive/v$JSONPATCH_VERSION.tar.gz -O ckanext-jsonpatch.tar.gz
+tar xzf ckanext-jsonpatch.tar.gz
+cd ckanext-jsonpatch
 python setup.py develop
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
