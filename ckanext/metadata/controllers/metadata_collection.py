04cda041eb2d182553b7e2c64d8341b3394cec4b
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-11-16 17:01:20 +0200

    Implement CRUD ops in metadata collection UIs

--- ckanext/metadata/controllers/metadata_collection.py
+++ ckanext/metadata/controllers/metadata_collection.py
@@ -7,6 +7,8 @@ from ckan.controllers.group import GroupController
 from ckan.lib.render import TemplateNotFound
 import ckan.model as model
 import ckan.lib.helpers as helpers
+from ckan.logic import clean_dict, tuplize_dict, parse_params
+import ckan.lib.navl.dictization_functions as dict_fns
 
 
 class MetadataCollectionController(GroupController):
@@ -50,21 +52,32 @@ class MetadataCollectionController(GroupController):
         except TemplateNotFound:
             return tk.render(template_name, extra_vars={'group_type': group_type})
 
-    def _get_org_dict(self, organization_id):
-        context = {'model': model, 'session': model.Session, 'user': tk.c.user}
-        data_dict = {'id': organization_id}
-        try:
-            return tk.get_action('organization_show')(context, data_dict)
-        except tk.NotFound:
-            tk.abort(404, tk._('Group not found'))
-        except tk.NotAuthorized:
-            tk.abort(403, tk._('Not authorized to see this page'))
+    def _redirect_to_this_controller(self, *args, **kw):
+        if tk.c.org_dict:
+            kw['organization_id'] = tk.c.org_dict['name']
+        return super(MetadataCollectionController, self)._redirect_to_this_controller(*args, **kw)
 
-    def index(self, organization_id):
+    def _url_for_this_controller(self, *args, **kw):
+        if tk.c.org_dict:
+            kw['organization_id'] = tk.c.org_dict['name']
+        return super(MetadataCollectionController, self)._redirect_to_this_controller(*args, **kw)
+
+    def _set_organization_context(self, organization_id):
+        if organization_id and not tk.c.org_dict:
+            context = {'model': model, 'session': model.Session, 'user': tk.c.user}
+            data_dict = {'id': organization_id}
+            try:
+                tk.c.org_dict = tk.get_action('organization_show')(context, data_dict)
+            except tk.ObjectNotFound:
+                tk.abort(404, tk._('Organization not found'))
+            except tk.NotAuthorized:
+                tk.abort(403, tk._('Not authorized to see this page'))
+
+    def index(self, organization_id=None):
         """
         Copied from GroupController.index() with some modifications.
         """
-        tk.c.org_dict = self._get_org_dict(organization_id)
+        self._set_organization_context(organization_id)
 
         page = tk.h.get_page_number(tk.request.params) or 1
         items_per_page = 21
@@ -127,14 +140,76 @@ class MetadataCollectionController(GroupController):
         return tk.render(self._index_template('metadata_collection'),
                          extra_vars={'group_type': 'metadata_collection'})
 
-    def read(self, organization_id, id, limit=20):
-        tk.c.org_dict = self._get_org_dict(organization_id)
+    def new(self, data=None, errors=None, error_summary=None, organization_id=None):
+        self._set_organization_context(organization_id)
+        return super(MetadataCollectionController, self).new(data, errors, error_summary)
+
+    def edit(self, id, data=None, errors=None, error_summary=None, organization_id=None):
+        self._set_organization_context(organization_id)
+        return super(MetadataCollectionController, self).edit(id, data, errors, error_summary)
+
+    def read(self, id, limit=20, organization_id=None):
+        self._set_organization_context(organization_id)
         return super(MetadataCollectionController, self).read(id, limit)
 
-    def activity(self, organization_id, id, offset=0):
-        tk.c.org_dict = self._get_org_dict(organization_id)
+    def activity(self, id, offset=0, organization_id=None):
+        self._set_organization_context(organization_id)
         return super(MetadataCollectionController, self).activity(id, offset)
 
-    def about(self, organization_id, id):
-        tk.c.org_dict = self._get_org_dict(organization_id)
+    def about(self, id, organization_id=None):
+        self._set_organization_context(organization_id)
         return super(MetadataCollectionController, self).about(id)
+
+    def members(self, id, organization_id=None):
+        self._set_organization_context(organization_id)
+        return super(MetadataCollectionController, self).members(id)
+
+    def member_new(self, id, organization_id=None):
+        self._set_organization_context(organization_id)
+        return super(MetadataCollectionController, self).member_new(id)
+
+    def member_delete(self, id, organization_id=None):
+        self._set_organization_context(organization_id)
+        return super(MetadataCollectionController, self).member_delete(id)
+
+    def _save_new(self, context, group_type=None):
+        """
+        Replacing GroupController._save_new because the redirect_to call is not wrapped.
+        """
+        try:
+            data_dict = clean_dict(dict_fns.unflatten(tuplize_dict(parse_params(tk.request.params))))
+            data_dict['type'] = 'metadata_collection'
+            context['message'] = data_dict.get('log_message', '')
+            data_dict['users'] = [{'name': tk.c.user, 'capacity': 'admin'}]
+            group = tk.get_action('metadata_collection_create')(context, data_dict)
+            tk.h.redirect_to('metadata_collection_read', id=group['name'], organization_id=tk.c.org_dict['name'])
+        except (tk.ObjectNotFound, tk.NotAuthorized), e:
+            tk.abort(404, tk._('Group not found'))
+        except dict_fns.DataError:
+            tk.abort(400, tk._(u'Integrity Error'))
+        except tk.ValidationError, e:
+            errors = e.error_dict
+            error_summary = e.error_summary
+            return self.new(data_dict, errors, error_summary)
+
+    def _save_edit(self, id, context):
+        """
+        Replacing GroupController._save_edit because the redirect_to call is not wrapped.
+        """
+        try:
+            data_dict = clean_dict(dict_fns.unflatten(tuplize_dict(parse_params(tk.request.params))))
+            context['message'] = data_dict.get('log_message', '')
+            data_dict['id'] = id
+            context['allow_partial_update'] = True
+            group = tk.get_action('metadata_collection_update')(context, data_dict)
+            if id != group['name']:
+                self._force_reindex(group)
+            tk.h.redirect_to('metadata_collection_read', id=group['name'], organization_id=tk.c.org_dict['name'])
+        except (tk.ObjectNotFound, tk.NotAuthorized), e:
+            tk.abort(404, tk._('Group not found'))
+        except dict_fns.DataError:
+            tk.abort(400, tk._(u'Integrity Error'))
+        except tk.ValidationError, e:
+            errors = e.error_dict
+            error_summary = e.error_summary
+            return self.edit(id, data_dict, errors, error_summary)
