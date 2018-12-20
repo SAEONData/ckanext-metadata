3f12a465a360d894f302a1a3cf377a18a16888c7
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-12-20 09:22:21 +0200

    Remove metadata collection index page and breadcrumb component
    The metadata collection index is already effectively on the organization UI; having an additional index page is redundant and adds complexity to the UI.

--- ckanext/metadata/controllers/metadata_collection.py
+++ ckanext/metadata/controllers/metadata_collection.py
@@ -74,70 +74,7 @@ class MetadataCollectionController(GroupController):
                 tk.abort(403, tk._('Not authorized to see this page'))
 
     def index(self, organization_id=None):
-        """
-        Copied from GroupController.index() with some modifications.
-        """
-        self._set_organization_context(organization_id)
-
-        page = tk.h.get_page_number(tk.request.params) or 1
-        items_per_page = 21
-
-        context = {'model': model, 'session': model.Session, 'user': tk.c.user,
-                   'for_view': True, 'with_private': True}
-
-        q = tk.c.q = tk.request.params.get('q', '')
-        sort_by = tk.c.sort_by_selected = tk.request.params.get('sort')
-        try:
-            tk.check_access('site_read', context)
-            tk.check_access('metadata_collection_list', context)
-        except tk.NotAuthorized:
-            tk.abort(403, tk._('Not authorized to see this page'))
-
-        if tk.c.userobj:
-            context['user_id'] = tk.c.userobj.id
-            context['user_is_admin'] = tk.c.userobj.sysadmin
-
-        try:
-            data_dict_global_results = {
-                'owner_org': organization_id,
-                'all_fields': False,
-                'q': q,
-                'sort': sort_by,
-                'type': 'metadata_collection',
-            }
-            global_results = tk.get_action('metadata_collection_list')(context, data_dict_global_results)
-        except tk.ValidationError as e:
-            if e.error_dict and e.error_dict.get('message'):
-                msg = e.error_dict['message']
-            else:
-                msg = str(e)
-            tk.h.flash_error(msg)
-            tk.c.page = helpers.Page([], 0)
-            return tk.render(self._index_template('metadata_collection'),
-                             extra_vars={'group_type': 'metadata_collection'})
-
-        data_dict_page_results = {
-            'owner_org': organization_id,
-            'all_fields': True,
-            'q': q,
-            'sort': sort_by,
-            'type': 'metadata_collection',
-            'limit': items_per_page,
-            'offset': items_per_page * (page - 1),
-            'include_extras': True
-        }
-        page_results = tk.get_action('metadata_collection_list')(context, data_dict_page_results)
-
-        tk.c.page = helpers.Page(
-            collection=global_results,
-            page=page,
-            url=tk.h.pager_url,
-            items_per_page=items_per_page,
-        )
-
-        tk.c.page.items = page_results
-        return tk.render(self._index_template('metadata_collection'),
-                         extra_vars={'group_type': 'metadata_collection'})
+        tk.h.redirect_to('organization_read', id=organization_id)
 
     def new(self, data=None, errors=None, error_summary=None, organization_id=None):
         self._set_organization_context(organization_id)
