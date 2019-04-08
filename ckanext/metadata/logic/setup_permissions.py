5318e912a584bde250b09becdfe142b2a7600ad9
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2019-04-08 14:02:38 +0200

    Refine contenttype-operation-actions permissions mappings

--- ckanext/metadata/logic/setup_permissions.py
+++ ckanext/metadata/logic/setup_permissions.py
@@ -4,107 +4,117 @@ import ckan.plugins.toolkit as tk
 from ckan import model
 
 _permissions_map = {
-    'metadata_record': {
-        'read': [
-            'metadata_record_show',
+    'metadata': {
+        'view': [
             'metadata_record_list',
-            'metadata_record_validation_schema_list',
-            'metadata_record_workflow_annotation_show',
-            'metadata_record_workflow_annotation_list',
-            'metadata_record_index_show',
+            'metadata_record_show',
+            'metadata_collection_show',
+            'organization_show',
+            'infrastructure_show',
+            'metadata_standard_show',
+            'workflow_state_show',
         ],
-        'write': [
+        'submit': [
+            'metadata_record_create',
+            'metadata_standard_list',
+            'metadata_standard_show',
+            'infrastructure_list',
+            'infrastructure_show',
+        ],
+        'manage': [
             'metadata_record_create',
             'metadata_record_update',
             'metadata_record_delete',
-        ],
-        'manage_workflow': [
+            'metadata_record_validation_schema_list',
             'metadata_record_validate',
             'metadata_record_invalidate',
+            'metadata_record_workflow_annotation_show',
+            'metadata_record_workflow_annotation_list',
             'metadata_record_workflow_annotation_create',
             'metadata_record_workflow_annotation_update',
             'metadata_record_workflow_annotation_delete',
             'metadata_record_workflow_state_transition',
             'metadata_record_workflow_state_revert',
+            'metadata_record_index_show',
             'metadata_record_index_update',
+            'metadata_standard_list',
+            'metadata_standard_show',
+            'infrastructure_list',
+            'infrastructure_show',
         ],
     },
 
-    'metadata_collection': {
-        'read': [
-            'metadata_collection_show',
+    'collections': {
+        'view': [
             'metadata_collection_list',
+            'metadata_collection_show',
+            'organization_show',
         ],
-        'write': [
+        'manage': [
             'metadata_collection_create',
             'metadata_collection_update',
             'metadata_collection_delete',
         ],
     },
 
-    'organization': {
-        'read': [
-            'organization_show',
+    'organizations': {
+        'view': [
             'organization_list',
+            'organization_show',
         ],
-        'write': [
+        'manage': [
             'organization_create',
             'organization_update',
             'organization_delete',
         ],
     },
 
-    'infrastructure': {
-        'read': [
-            'infrastructure_show',
+    'infrastructures': {
+        'view': [
             'infrastructure_list',
+            'infrastructure_show',
         ],
-        'write': [
+        'manage': [
             'infrastructure_create',
             'infrastructure_update',
             'infrastructure_delete',
         ],
     },
 
-    'metadata_standard': {
-        'read': [
-            'metadata_standard_show',
+    'standards': {
+        'view': [
             'metadata_standard_list',
-            'metadata_standard_index_show',
+            'metadata_standard_show',
+            'metadata_schema_list',
+            'metadata_schema_show',
+            'infrastructure_show',
+            'organization_show',
+            'metadata_json_attr_map_list',
+            'metadata_json_attr_map_show',
         ],
-        'write': [
+        'configure': [
             'metadata_standard_create',
             'metadata_standard_update',
             'metadata_standard_delete',
-        ],
-        'manage_search_index': [
-            'metadata_standard_index_create',
-            'metadata_standard_index_delete',
-        ]
-    },
-
-    'metadata_schema': {
-        'read': [
-            'metadata_schema_show',
-            'metadata_schema_list',
-        ],
-        'write': [
             'metadata_schema_create',
             'metadata_schema_update',
             'metadata_schema_delete',
+            'metadata_json_attr_map_create',
+            'metadata_json_attr_map_update',
+            'metadata_json_attr_map_delete',
         ],
     },
 
-    'workflow_config': {
-        'read': [
-            'workflow_state_show',
+    'workflows': {
+        'view': [
             'workflow_state_list',
-            'workflow_transition_show',
+            'workflow_state_show',
             'workflow_transition_list',
-            'workflow_annotation_show',
+            'workflow_transition_show',
             'workflow_annotation_list',
+            'workflow_annotation_show',
         ],
-        'write': [
+        'configure': [
             'workflow_state_create',
             'workflow_state_update',
             'workflow_state_delete',
@@ -113,6 +123,17 @@ _permissions_map = {
             'workflow_annotation_create',
             'workflow_annotation_update',
             'workflow_annotation_delete',
+        ],
+    },
+
+    'search_catalogue': {
+        'view': [
+            'metadata_standard_index_show',
+            'metadata_standard_show',
+        ],
+        'manage': [
+            'metadata_standard_index_create',
+            'metadata_standard_index_delete',
         ],
     },
 }
